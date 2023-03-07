d12045e1af358af5d123f3391dfc0b8d16f10c04
Lauren Johnston <hilotraining@outlook.com>
2023-03-06 21:11:29 -0700

    done exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,14 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
+
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Lauren", last_name: "Johnston", hourly_rate: 18)
+@store1.employees.create(first_name: "Garrett", last_name: "King", hourly_rate: 30)
+@store2.employees.create(first_name: "Yennefer", last_name: "Elizabeth", hourly_rate: 25)
+@store2.employees.create(first_name: "Jocelyn", last_name: "Gillis", hourly_rate: 50)
+@store3.employees.create(first_name: "Alex", last_name: "Paris", hourly_rate: 25)
+@store3.employees.create(first_name: "Kyle", last_name: "Wayne", hourly_rate: 53)
+
